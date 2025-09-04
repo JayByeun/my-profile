10bc3699ce235ff3a6c9fb70d8f814cd0b1b5024
Jay.Byeun <jayay.byeun@gmail.com>
2025-09-04 16:34:57 -0500

    Added animation to home

--- src/components/nav/Home.tsx
+++ src/components/nav/Home.tsx
@@ -2,9 +2,33 @@ const Home = () => {
   return (
     <section
       id="home"
-      className="min-h-screen flex items-center justify-center bg-gray-100"
+      className="relative min-h-screen flex items-center justify-center overflow-hidden group group-hover:text-white"
     >
-      <div className="font-heading font-bold text-5xl text-red-500">
+      {/* Bottom half white */}
+      <div className="absolute bottom-0 left-0 w-full h-1/2 bg-white"></div>
+
+      {/* Top half rose with wave border */}
+      <div className="absolute bottom-0 left-0 w-full h-1/2 bg-rose-500 transition-all duration-700 ease-in-out group-hover:h-full">
+        {/* Wavy top border */}
+        <svg
+          className="absolute top-0 left-0 w-full h-50"
+          viewBox="0 0 1440 100"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M0,80 
+               C150,20 250,100 360,60 
+               C480,20 600,100 720,50 
+               C840,0 960,80 1080,40 
+               C1200,0 1320,60 1440,20 
+               L1440,0 L0,0 Z"
+            fill="#FFFFFF" /* rose color */
+          />
+        </svg>
+      </div>
+
+      {/* Center text */}
+      <div className="relative z-10 font-heading font-bold text-5xl text-black group-hover:text-white transition-colors duration-700">
         Let's get to know!
       </div>
     </section>
--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -7,18 +7,27 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 w-full bg-white shadow-md z-50">
       <div className="max-w-6xl mx-auto flex justify-between items-center py-4 px-6">
-        <div className="font-bold text-xl">Jay Byeun</div>
+        <Link
+          to={"home"}
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          className="font-bold text-xl cursor-pointer hover:text-rose-400"
+        >
+          Jay Byeun
+        </Link>
         <ul className="flex space-x-6">
           {links.map((link) => (
             <li key={link}>
               <Link
-                activeClass="text-red-500"
+                activeClass="text-500"
                 to={link.toLowerCase()}
                 spy={true}
                 smooth={true}
                 offset={-70}
                 duration={500}
-                className="cursor-pointer hover:text-red-500"
+                className="cursor-pointer hover:text-rose-400"
               >
                 {link}
               </Link>
